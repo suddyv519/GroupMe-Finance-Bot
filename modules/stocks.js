c452c38b036aebee740e4d5a2074f7f9c471a0a6
Sudharshan Venkatesh <venkatesh.sudharshan@gmail.com>
2018-07-05 21:14:44 -0400

    changed 'toSearchFor' to 'trigger' for easier understanding

--- modules/stocks.js
+++ modules/stocks.js
@@ -1,11 +1,11 @@
 exports.process = (message, bot) => {
-    const toSearchFor = "@stock ";
-    const index = message.text.toLowerCase().indexOf(toSearchFor);
+    const trigger = "@stock ";
+    const index = message.text.toLowerCase().indexOf(trigger);
 
     //If we found the string we're looking for get results and send them
     if (index != -1) {
         //Get the string we are searching for.
-        const ticker = message.text.substring(index + toSearchFor.length);
+        const ticker = message.text.substring(index + trigger.length);
         console.log(ticker);
         //Get the total, encoded URL we're going to pass to IEX to search
         const stockurl = `https://api.iextrading.com/1.0/stock/${encodeURIComponent(ticker)}/price`;
