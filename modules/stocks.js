5f94b306ae1caaf46f32e70bbb6b993b990004dc
Sudharshan Venkatesh <venkatesh.sudharshan@gmail.com>
2020-03-08 15:10:08 -0400

    fixes and logging

--- modules/stocks.js
+++ modules/stocks.js
@@ -1,27 +1,25 @@
 exports.process = (message, bot) => {
-    const trigger = "@stock ";
-    const index = message.text.toLowerCase().indexOf(trigger);
     const key = process.env.API_KEY;
 
-    //If we found the string we're looking for get results and send them
     if (index != -1) {
-        //Get the string we are searching for.
         const tickers = message.text.match(/\B[$]\w\w\w\w?\s/g);
-
-        //Get the total, encoded URL we're going to pass to API to search
-        const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${encodeURIComponent(ticker.trim())}&apikey=${key}`;
+        console.log(tickers);
         
-        tickers.forEach(ticker => {
-            bot.request.get(stockurl, (error, response, body) => {
+        tickers.forEach(t => {
+            const ticker = encodeURIComponent(t.trim());
+            const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker}&apikey=${key}`;
+            bot.request.get(url, (error, response, body) => {
                 if (error) {
                     bot.sendMessage(`There was an error with the request`);
+                    console.log(error);
                 } else {
-                    const response = body['Global Quote'];
+                    const resp = body['Global Quote'];
+                    console.log(resp);
                     if (price === "Unknown symbol") {
                         bot.sendMessage(`No price data for ${ticker.toUpperCase()} found`);
                     } else {
                         // bot.sendMessage(`${ticker.toUpperCase()}: $${price}`);
-                        bot.sendMessage(body);
+                        bot.sendMessage(resp);
                     }
                 }
             });
