ea0f7f1cb6071d36ac385910a521d9dee330d1f5
Sudharshan Venkatesh <venkatesh.sudharshan@gmail.com>
2020-03-08 15:22:07 -0400

    better logging

--- modules/stocks.js
+++ modules/stocks.js
@@ -1,20 +1,19 @@
 exports.process = (message, bot) => {
     const key = process.env.API_KEY;
-    console.log(message.text);
     const tickers = message.text.match(/\B[$]\w\w\w\w?\s?/g);
-    console.log(tickers);
 
     if (tickers && tickers.length > 0) {
         tickers.forEach(t => {
             const ticker = encodeURIComponent(t.slice(1).trim());
+            console.log(ticker);
             const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker}&apikey=${key}`;
             bot.request.get(url, (error, response, body) => {
                 if (error) {
                     bot.sendMessage(`There was an error with the request`);
                     console.log(error);
                 } else {
+                    console.log(body);
                     const resp = body['Global Quote'];
-                    console.log(resp);
                     // bot.sendMessage(`${ticker.toUpperCase()}: $${price}`);
                     bot.sendMessage(resp);
                 }
