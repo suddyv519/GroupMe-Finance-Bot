297cddca821d2bef9c5a8f4e8ad6b05c81936df9
Sudharshan Venkatesh <venkatesh.sudharshan@gmail.com>
2020-03-08 14:58:23 -0400

    change api, support multiple tickers

--- modules/stocks.js
+++ modules/stocks.js
@@ -1,29 +1,30 @@
 exports.process = (message, bot) => {
     const trigger = "@stock ";
     const index = message.text.toLowerCase().indexOf(trigger);
+    const key = process.env.API_KEY;
 
     //If we found the string we're looking for get results and send them
     if (index != -1) {
         //Get the string we are searching for.
-        const ticker = message.text.substring(index + trigger.length).trim();
-        //Get the total, encoded URL we're going to pass to IEX to search
-        const stockurl = `https://api.iextrading.com/1.0/stock/${encodeURIComponent(ticker)}/price`;
+        const tickers = message.text.match(/\B[$]\w\w\w\w?\s/g);
+
+        //Get the total, encoded URL we're going to pass to API to search
+        const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${encodeURIComponent(ticker.trim())}&apikey=${key}`;
         
-        //only send request if ticker is a proper length
-        if (ticker.length <= 5) {
-            //Get the IEX result, and send it, if found
+        tickers.forEach(ticker => {
             bot.request.get(stockurl, (error, response, body) => {
                 if (error) {
                     bot.sendMessage(`There was an error with the request`);
                 } else {
-                    const price = body;
+                    const response = body['Global Quote'];
                     if (price === "Unknown symbol") {
                         bot.sendMessage(`No price data for ${ticker.toUpperCase()} found`);
                     } else {
-                        bot.sendMessage(`${ticker.toUpperCase()}: $${price}`);
+                        // bot.sendMessage(`${ticker.toUpperCase()}: $${price}`);
+                        bot.sendMessage(body);
                     }
                 }
             });
-        }
+        });
     }
 };
